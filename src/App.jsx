852d29d8b3124aa0d5a6eef3d400f5aead5dfcef
Maria Vanegas <francy.mvanegas@gmail.com>
2024-08-14 13:12:41 -0500

    first commit parcial1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Card from "./components/card";
+
+function App() {
+    return (
+      <div className="App">
+        <h1>carga de estudiantes</h1>
+        <form></form>
+        <Card />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/card.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Card =() => {
+    return <div>hola, soy una tarjeta</div>
+};
+
+export default Card;
